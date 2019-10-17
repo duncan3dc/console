5894942c8044a4a5e979ad3d664887ea198de74a
Craig Duncan <git@duncanc.co.uk>
2019-10-17 05:40:24 +0100

    Ensure that uopz doesn't muck about with the exit opcode

--- tests/app.php
+++ tests/app.php
@@ -5,6 +5,9 @@ use duncan3dc\Console\Command;
 
 require __DIR__ . "/../vendor/autoload.php";
 
+# Ensure that uopz doesn't muck about with the exit opcode
+uopz_allow_exit(true);
+
 $application = new Application();
 
 $application->loadCommands(__DIR__ . "/commands/base");
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,6 @@
 <?php
 
 require __DIR__ . "/../vendor/autoload.php";
+
+# Ensure that uopz doesn't muck about with the exit opcode
+uopz_allow_exit(true);
